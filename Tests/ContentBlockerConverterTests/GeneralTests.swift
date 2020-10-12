8a7c32227559f00a815c3703bdb29e235c5ba72c
mizzick <ds.kolyshev@gmail.com>
2020-10-12 22:34:39 +0200

    performance: findCosmeticRuleMarker

--- Tests/ContentBlockerConverterTests/GeneralTests.swift
+++ Tests/ContentBlockerConverterTests/GeneralTests.swift
@@ -330,7 +330,7 @@ final class GeneralTests: XCTestCase {
         let content = try! String(contentsOf: resourceURL, encoding: String.Encoding.utf8);
         let rules = content.components(separatedBy: "\n");
         
-        // Average time 3.1 sec
+        // Average time 4.7 sec
         self.measure {
             let conversionResult = ContentBlockerConverter().convertArray(rules: rules);
             NSLog(conversionResult!.message);
