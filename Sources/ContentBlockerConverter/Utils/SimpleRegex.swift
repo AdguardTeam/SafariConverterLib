dee90444000ae34c7e081b59c3818e910acfe2eb
Andrey Meshkov <am@adguard.com>
2020-10-14 19:14:30 +0300

    Speed up createRegexText

--- Sources/ContentBlockerConverter/Utils/SimpleRegex.swift
+++ Sources/ContentBlockerConverter/Utils/SimpleRegex.swift
@@ -24,71 +24,13 @@ class SimpleRegex {
     private static let regexStartUrl = URL_FILTER_REGEXP_START_URL;
     private static let regexStartString = "^";
     private static let regexEndString = "$";
-    
+
     /**
-    * Creates regex
-    */
-    public static func createRegexText(str: String) -> String? {
-        if (str == maskStartUrl ||
-            str == maskPipe ||
-            str == maskAnySymbol) {
-            return regexAnySymbol;
-        }
-
-        var regex = SimpleRegex.escapeRegExp(str: str) as NSString;
-
-        var replaced: String;
-        if (regex.hasPrefix(maskStartUrl)) {
-            let substring = (regex.substring(to: regex.length - 1) as NSString).substring(from: maskStartUrl.count);
-            replaced = regex.substring(to: maskStartUrl.count) +
-                replaceAll(str: substring, find: "|", replace: "\\|") +
-                regex.substring(from: regex.length - 1);
-        } else if (regex.hasPrefix(maskPipe)) {
-            let substring = (regex.substring(to: regex.length - 1) as NSString).substring(from: maskPipe.count);
-            replaced = regex.substring(to: maskPipe.count) +
-                replaceAll(str: substring, find: "|", replace: "\\|") +
-                regex.substring(from: regex.length - 1);
-        } else {
-            let substring = regex.substring(to: regex.length - 1);
-            replaced = replaceAll(str: substring, find: "|", replace: "\\|") +
-                regex.substring(from: regex.length - 1);
-        }
-
-        // Replacing special url masks
-        regex = replaceAll(str: replaced, find: maskAnySymbol, replace: regexAnySymbol) as NSString;
-        regex = replaceAll(str: regex as String, find: maskSeparator, replace: regexSeparator) as NSString;
-
-        if (regex.hasPrefix(maskStartUrl)) {
-            regex = regexStartUrl + regex.substring(from: maskStartUrl.count) as NSString;
-        } else if (regex.hasPrefix(maskPipe)) {
-            regex = regexStartString + regex.substring(from: maskPipe.count) as NSString;
-        }
-        
-        if (regex.hasSuffix(maskPipe)) {
-            regex = regex.substring(to: regex.length - 1) + regexEndString as NSString;
-        }
-
-        return regex as String;
-    }
-    
-    /**
-     * Check if target string matches regex
+     * Characters to be escaped in the regex
+     * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/regexp
+     * should be escaped . * + ? ^ $ { } ( ) | [ ] / \
+     * except of * | ^
      */
-    static func isMatch(regex: NSRegularExpression, target: String) -> Bool {
-        let matchCount = regex.numberOfMatches(in: target, options: [], range: NSMakeRange(0, target.count))
-        return matchCount > 0;
-    }
-    
-    /**
-     * Returns target string matches
-     */
-    static func matches(regex: NSRegularExpression, target: String) -> [String] {
-        let matches  = regex.matches(in: target, options: [], range: NSMakeRange(0, target.count))
-        return matches.map { match in
-            return String(target[Range(match.range, in: target)!])
-        }
-    }
-    
     private static let CHARS_TO_ESCAPE = [
         ".".utf16.first!,
         "?".utf16.first!,
@@ -101,32 +43,78 @@ class SimpleRegex {
         "]".utf16.first!,
         "/".utf16.first!
     ]
-    
+    private static let charPipe = "|".utf16.first!
+    private static let charSeparator = "^".utf16.first!
+    private static let charWildcard = "*".utf16.first!
+
     /**
-     * Escapes regular expression string
-     * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/regexp
-     * should be escaped . * + ? ^ $ { } ( ) | [ ] / \
-     * except of * | ^
+     * Creates regex
      */
-    private static func escapeRegExp(str: String) -> String {
-        let nsstring = str as NSString;
-        let maxIndex = nsstring.length - 1;
-        
-        var result = "";
-        
-        for index in 0...maxIndex {
-            let char = nsstring.character(at: index)
-            if (CHARS_TO_ESCAPE.contains(char)) {
-                result.append("\\");
-            }
-            
-            result.append(Character(UnicodeScalar(char)!));
+    public static func createRegexText(str: String) -> String? {
+        if (str == maskStartUrl ||
+                str == maskPipe ||
+                str == maskAnySymbol) {
+            return regexAnySymbol;
         }
         
+        var result = ""
+        let nstr = str as NSString
+        
+        let maxIndex = nstr.length - 1
+        var i = 0
+        while i <= maxIndex {
+            let char = nstr.character(at: i)
+
+            if CHARS_TO_ESCAPE.contains(char) {
+                result.append("\\")
+                result.append(Character(UnicodeScalar(char)!))
+            } else {
+                switch char {
+                case charPipe:
+                    if i == 0 {
+                        let nextChar = nstr.character(at: i+1)
+                        if nextChar == charPipe {
+                            result.append(regexStartUrl)
+                            i += 1 // increment i as we processed next char already
+                        } else {
+                            result.append(regexStartString)
+                        }
+                    } else if i == maxIndex {
+                        result.append(regexEndString)
+                    } else {
+                        result.append("\\")
+                        result.append(Character(UnicodeScalar(char)!))
+                    }
+                case charSeparator:
+                    result.append(regexSeparator)
+                case charWildcard:
+                    result.append(regexAnySymbol)
+                default:
+                    result.append(Character(UnicodeScalar(char)!))
+                }
+            }
+            
+            i += 1
+        }
+
         return result;
     }
-    
-    private static func replaceAll(str: String, find: String, replace: String) -> String {
-        return (str as NSString).replacingOccurrences(of: find, with: replace);
+
+    /**
+     * Check if target string matches regex
+     */
+    static func isMatch(regex: NSRegularExpression, target: String) -> Bool {
+        let matchCount = regex.numberOfMatches(in: target, options: [], range: NSMakeRange(0, target.count))
+        return matchCount > 0;
+    }
+
+    /**
+     * Returns target string matches
+     */
+    static func matches(regex: NSRegularExpression, target: String) -> [String] {
+        let matches  = regex.matches(in: target, options: [], range: NSMakeRange(0, target.count))
+        return matches.map { match in
+            return String(target[Range(match.range, in: target)!])
+        }
     }
 }
