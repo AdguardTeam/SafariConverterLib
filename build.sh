d6fbeeaa0a709f61a952ba4f8f18de09188e021f
tvinzz <tvinzz@tvinzzs-MacBook-Pro.local>
2020-12-15 03:47:10 +0300

    added ConverterTool.json

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 swift build -v -c release
 mkdir -p bin
 cp .build/release/ConverterTool bin
+
+LIB_VERSION=$(node -p -e "require('./package.json').version")
+touch bin/ConverterTool.json
+echo "{\"version\": \"$LIB_VERSION\"}" > bin/ConverterTool.json
