5e43555384aac9735f648724c4b4d1be080fa36f
mizzick <ds.kolyshev@gmail.com>
2020-12-22 21:58:02 +0200

    feature:AG-5471 tld list extended to 200 items

--- Tests/ContentBlockerConverterTests/ContentBlockerConverterTests.swift
+++ Tests/ContentBlockerConverterTests/ContentBlockerConverterTests.swift
@@ -481,31 +481,35 @@ final class ContentBlockerConverterTests: XCTestCase {
     
     func testTldWildcardRules() {
         var result = converter.convertArray(rules: ["surge.*,testcases.adguard.*###case-5-wildcard-for-tld > .test-banner"]);
-        XCTAssertEqual(result?.convertedCount, 1);
+        XCTAssertEqual(result?.convertedCount, 2);
         
         var decoded = try! parseJsonString(json: result!.converted);
-        XCTAssertEqual(decoded.count, 1);
+        XCTAssertEqual(decoded.count, 2);
         XCTAssertEqual(decoded[0].trigger.urlFilter, URL_FILTER_CSS_RULES);
         XCTAssertEqual(decoded[0].trigger.ifDomain?[0], "*surge.com");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[1], "*surge.org");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[2], "*surge.ru");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[99], "*surge.sh");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[100], "*testcases.adguard.com");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[101], "*testcases.adguard.org");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?[1], "*surge.ru");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?[2], "*surge.net");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?.count, 250);
+        
+        XCTAssertEqual(decoded[1].trigger.urlFilter, URL_FILTER_CSS_RULES);
+        XCTAssertNotNil(decoded[1].trigger.ifDomain?[0]);
+        XCTAssertEqual(decoded[1].trigger.ifDomain?.count, 150);
         
         
         result = converter.convertArray(rules: ["||*/test-files/adguard.png$domain=surge.*|testcases.adguard.*"]);
-        XCTAssertEqual(result?.convertedCount, 1);
+        XCTAssertEqual(result?.convertedCount, 2);
         
         decoded = try! parseJsonString(json: result!.converted);
-        XCTAssertEqual(decoded.count, 1);
+        XCTAssertEqual(decoded.count, 2);
         XCTAssertEqual(decoded[0].trigger.urlFilter, START_URL_UNESCAPED + ".*\\/test-files\\/adguard\\.png");
         XCTAssertEqual(decoded[0].trigger.ifDomain?[0], "*surge.com");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[1], "*surge.org");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[2], "*surge.ru");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[99], "*surge.sh");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[100], "*testcases.adguard.com");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[101], "*testcases.adguard.org");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?[1], "*surge.ru");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?[2], "*surge.net");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?.count, 250);
+        
+        XCTAssertEqual(decoded[1].trigger.urlFilter, START_URL_UNESCAPED + ".*\\/test-files\\/adguard\\.png");
+        XCTAssertNotNil(decoded[1].trigger.ifDomain?[0]);
+        XCTAssertEqual(decoded[1].trigger.ifDomain?.count, 150);
         
         result = converter.convertArray(rules: ["|http$script,domain=forbes.*"]);
         XCTAssertEqual(result?.convertedCount, 1);
@@ -516,9 +520,10 @@ final class ContentBlockerConverterTests: XCTestCase {
         XCTAssertEqual(decoded[0].trigger.urlFilter, "^http");
         XCTAssertEqual(decoded[0].trigger.resourceType, ["script"]);
         XCTAssertEqual(decoded[0].trigger.ifDomain?[0], "*forbes.com");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[1], "*forbes.org");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[2], "*forbes.ru");
-        XCTAssertEqual(decoded[0].trigger.ifDomain?[99], "*forbes.sh");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?[1], "*forbes.ru");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?[2], "*forbes.net");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?[199], "*forbes.sh");
+        XCTAssertEqual(decoded[0].trigger.ifDomain?.count, 200);
     }
 
     func testUboScriptletRules() {
