505890a71b5f7d99d18ca086259743a40e2bf434
mizzick <ds.kolyshev@gmail.com>
2021-03-02 23:22:33 +0200

    fix:AG-6817 removed embarrassing rule test

--- Tests/ContentBlockerConverterTests/ContentBlockerConverterTests.swift
+++ Tests/ContentBlockerConverterTests/ContentBlockerConverterTests.swift
@@ -421,14 +421,6 @@ final class ContentBlockerConverterTests: XCTestCase {
         XCTAssertEqual(result?.convertedCount, 0);
         XCTAssertEqual(result?.errorsCount, 1);
 
-        ruleText = "^https?://(?!static)([^.]+)+?fastpicru[:/]$script,domain=fastpic.ru";
-        result = converter.convertArray(rules: [ruleText]);
-        XCTAssertEqual(result?.convertedCount, 1);
-        XCTAssertEqual(result?.errorsCount, 0);
-
-        let decoded = try! parseJsonString(json: result!.converted);
-        XCTAssertEqual(decoded[0].trigger.urlFilter, URL_FILTER_REGEXP_SEPARATOR + "https\\?:\\/\\/\\(\\?!static\\)\\(\\[" + URL_FILTER_REGEXP_SEPARATOR + "\\.\\]\\+\\)\\+\\?fastpicru\\[:\\/\\]");
-
         ruleText = #"@@/:\/\/.*[.]wp[.]pl\/[a-z0-9_]{30,50}[.][a-z]{2,5}([\/:&\?].*)?$/"#;
         result = converter.convertArray(rules: [ruleText]);
         XCTAssertEqual(result?.convertedCount, 0);
