3398ce0c16af28a156ecdeb6d147502f4bea7853
tvinzz <o.labudko@adguard.com>
2021-04-13 00:18:04 +0200

    build binary for both archs

--- node-safari-converter/script/build.sh
+++ node-safari-converter/script/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-swift build -v -c release
-mkdir -p bin
-cp .build/release/ConverterTool bin
+mkdir -p bin/x64
+mkdir -p bin/arm64
+
+swift build -v -c release --arch x86_64
+cp .build/x86_64-apple-macosx/release/ConverterTool bin/x64
+
+swift build -v -c release --arch arm64
+cp .build/arm64-apple-macosx/release/ConverterTool bin/arm64
+
+rm -rf .build
