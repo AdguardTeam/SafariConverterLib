3ada7822de30b88aef53a155f3f6973256b26fad
tvinzz <o.labudko@adguard.com>
2021-04-14 18:24:50 +0200

    removed lipo

--- node-safari-converter/script/build.sh
+++ node-safari-converter/script/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir -p bin
+mkdir -p bin/x64
+mkdir -p bin/arm64
 
 swift build -v -c release --arch x86_64
-swift build -v -c release --arch arm64
+cp .build/x86_64-apple-macosx/release/ConverterTool bin/x64
 
-lipo -create .build/x86_64-apple-macosx/release/ConverterTool .build/arm64-apple-macosx/release/ConverterTool -output bin/ConverterTool
+swift build -v -c release --arch arm64
+cp .build/arm64-apple-macosx/release/ConverterTool bin/arm64
 
 rm -rf .build
