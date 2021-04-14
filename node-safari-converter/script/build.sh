ac0d39e3bc5dbfc8a2f0d9060a8dffb082fd035c
tvinzz <o.labudko@adguard.com>
2021-04-14 16:48:20 +0200

    made universal binary with lipo

--- node-safari-converter/script/build.sh
+++ node-safari-converter/script/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-mkdir -p bin/x64
-mkdir -p bin/arm64
+mkdir -p bin
 
 swift build -v -c release --arch x86_64
-cp .build/x86_64-apple-macosx/release/ConverterTool bin/x64
-
 swift build -v -c release --arch arm64
-cp .build/arm64-apple-macosx/release/ConverterTool bin/arm64
+
+lipo -create .build/x86_64-apple-macosx/release/ConverterTool .build/arm64-apple-macosx/release/ConverterTool -output bin/ConverterTool
 
 rm -rf .build
