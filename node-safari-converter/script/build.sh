92480fad14014c5bd8f84010cf74e27bf23436c8
tvinzz <o.labudko@adguard.com>
2021-04-15 02:26:51 +0200

    made fat binary

--- node-safari-converter/script/build.sh
+++ node-safari-converter/script/build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-mkdir -p bin/x64
-mkdir -p bin/arm64
+mkdir -p bin
 
-swift build -v -c release --arch x86_64
-cp .build/x86_64-apple-macosx/release/ConverterTool bin/x64
-
-swift build -v -c release --arch arm64
-cp .build/arm64-apple-macosx/release/ConverterTool bin/arm64
+swift build -v -c release --arch arm64 --arch x86_64
+cp .build/apple/Products/Release/ConverterTool bin
 
 rm -rf .build
