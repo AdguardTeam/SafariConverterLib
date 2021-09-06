4bf4e8c220b40829270f889db4f8bea7f42394f5
tvinzz <o.labudko@adguard.com>
2021-09-06 19:37:28 +0300

    fixed conflicts

--- Sources/CommandLineWrapper/main.swift
+++ Sources/CommandLineWrapper/main.swift
@@ -27,50 +27,6 @@ func encodeJson(_ result: ConversionResult) throws -> String {
     return String(data: json, encoding: .utf8)!.replacingOccurrences(of: "\\/", with: "/")
 }
 
-do {
-    Logger.log("AG: Conversion started");
-
-    let arguments: [String] = CommandLine.arguments;
-    if (arguments.count < 4) {
-        writeToStdError(str: "AG: Invalid arguments: Usage: ./CommandLineWrapper -safariVersion=14 -optimize=false -advancedBlocking)");
-        exit(EXIT_FAILURE);
-    }
-
-    let safariVersionIndex = String.Index(utf16Offset: 15, in: arguments[1]);
-    let safariVersionStr = arguments[1][safariVersionIndex...];
-
-    guard let safariVersionNum = Int(safariVersionStr) else {
-        throw SafariVersionError.invalidSafariVersion(version: String(safariVersionStr));
-    };
-
-    guard let safariVersion = SafariVersion(rawValue: safariVersionNum) else {
-        throw SafariVersionError.unsupportedSafariVersion(version: safariVersionNum);
-    };
-
-    Logger.log("AG: Safari version: \(safariVersion)");
-
-    let optimize = arguments[2] == "-optimize=true";
-    Logger.log("AG: Optimize: \(optimize)");
-
-    let advancedBlocking = arguments[3] == "-advancedBlocking=true";
-    Logger.log("AG: AdvancedBlocking: \(advancedBlocking)");
-
-    var rules = [String]();
-    var line: String? = nil;
-    while (true) {
-        line = readLine(strippingNewline: true);
-        if (line == nil || line == "") {
-            break;
-        }
-
-        rules.append(line!);
-    }
-
-    Logger.log("AG: Rules to convert: \(rules.count)");
-
-    let result: ConversionResult? = ContentBlockerConverter().convertArray(
-        rules: rules, safariVersion: safariVersion, optimize: optimize, advancedBlocking: advancedBlocking
-    );
 /**
  * Converter tool
  * Usage:
@@ -92,11 +48,11 @@ struct ConverterTool: ParsableCommand {
     var rules: String?
 
     mutating func run() throws {
-        guard let safariVersion = SafariVersion(rawValue: safariVersion) else {
-            throw SafariVersionError.unsupportedSafariVersion()
+        guard let safariVersionResolved = SafariVersion(rawValue: safariVersion) else {
+            throw SafariVersionError.unsupportedSafariVersion(version: safariVersion)
         }
 
-        Logger.log("Safari version - \(safariVersion)")
+        Logger.log("Safari version - \(safariVersionResolved)")
         Logger.log("Optimize - \(optimize)")
         Logger.log("Advanced blocking - \(advancedBlocking)")
 
@@ -116,7 +72,7 @@ struct ConverterTool: ParsableCommand {
         let result: ConversionResult? = ContentBlockerConverter()
             .convertArray(
                 rules: rules,
-                safariVersion: safariVersion,
+                safariVersion: safariVersionResolved,
                 optimize: optimize,
                 advancedBlocking: advancedBlocking
             )
