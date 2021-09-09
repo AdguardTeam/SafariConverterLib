865b3841117aa18b8f1eb00c7fd480ee4d1e56e1
tvinzz <o.labudko@adguard.com>
2021-09-09 18:35:24 +0300

    added userRuleIsAssociated and tests

--- Sources/ContentBlockerConverter/QuickAllowlistClipper.swift
+++ Sources/ContentBlockerConverter/QuickAllowlistClipper.swift
@@ -24,6 +24,9 @@ public protocol QuickAllowlistClipperProtocol {
     
     // Public method to check if inverted allowlist rules contain inverted allowlist rule for provided domain
     func invertedAllowlistContains(domain: String, _ invertedAllowlistRules: [String]) -> Bool
+    
+    // Public method to check if provided rule is associated with the domain
+    func userRuleIsAssociated(with domain: String, _ userRule: String) -> Bool
 }
 
 /**
@@ -164,6 +167,39 @@ public class QuickAllowlistClipper: QuickAllowlistClipperProtocol {
     public func invertedAllowlistContains(domain: String, _ invertedAllowlistRules: [String]) -> Bool {
         let invertedAllowlistRuleToFind = ContentBlockerConverter.createInvertedAllowlistRule(by: domain)
         return invertedAllowlistRules.contains { $0 == invertedAllowlistRuleToFind }
+    }
+    
+    /**
+     * Parses domains from provided rule
+     */
+    func parseRuleDomains(ruleText: String) -> [String] {
+        do {
+            let rule = try RuleFactory.createRule(ruleText: ruleText as NSString)
+            if rule == nil {
+                return []
+            }
+            
+            var ruleDomains = rule!.permittedDomains + rule!.restrictedDomains
+            
+            if !RuleFactory.isCosmetic(ruleText: ruleText as NSString) {
+                let ruleDomain = (rule! as! NetworkRule).parseRuleDomain()?.domain
+                if (ruleDomain != nil) {
+                    ruleDomains += [String(ruleDomain!)]
+                }
+            }
+            return ruleDomains;
+
+        } catch {
+            return []
+        }
+    }
+    
+    /**
+     * Checks if provided rule is associated with the domain
+     */
+    public func userRuleIsAssociated(with domain: String, _ userRule: String) -> Bool {
+        let ruleDomains = parseRuleDomains(ruleText: userRule)
+        return ruleDomains.contains{ $0 == domain }
     }
 }
 
--- Tests/ContentBlockerConverterTests/QuickAllowlistClipperTests.swift
+++ Tests/ContentBlockerConverterTests/QuickAllowlistClipperTests.swift
@@ -363,14 +363,64 @@ final class QuickAllowlistClipperTests: XCTestCase {
             "@@||*$document,domain=~test4.com",
         ];
         
-        let result1 = QuickAllowlistClipper().invertedAllowlistContains(domain: "test1.com", rulesText);
-        XCTAssertFalse(result1);
+        result = QuickAllowlistClipper().invertedAllowlistContains(domain: "test1.com", rulesText);
+        XCTAssertFalse(result);
         
-        let result2 = QuickAllowlistClipper().invertedAllowlistContains(domain: "test2.com", rulesText);
-        XCTAssertFalse(result2);
+        result = QuickAllowlistClipper().invertedAllowlistContains(domain: "test2.com", rulesText);
+        XCTAssertFalse(result);
         
-        let result3 = QuickAllowlistClipper().invertedAllowlistContains(domain: "test3.com", rulesText);
-        XCTAssertFalse(result3);
+        result = QuickAllowlistClipper().invertedAllowlistContains(domain: "test3.com", rulesText);
+        XCTAssertFalse(result);
+    }
+    
+    func testUserRuleIsAssociated() {
+        var result = QuickAllowlistClipper().userRuleIsAssociated(with: "test.com", "test.com##.logos");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "test.co", "test.co.th##.logos");
+        XCTAssertFalse(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "test.com", "||test.com^$script");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "test.com", "||freetest.com");
+        XCTAssertFalse(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "test.com", "||example.org^$script,domain=test.com");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "example.org", "||example.org^$script,domain=test.com");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "test.com", "!||test.com^$script");
+        XCTAssertFalse(result);
+        
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "test.com", "@@||test.com^");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "test1.com", "||example.org^$script,domain=test1.com|test2.com");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "example.org", "||1example.com^$image,domain=example.co|example.com.ru");
+        XCTAssertFalse(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "example.org", "||example.org/js/*$script,domain=test1.com|test2.com");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "example.org", "example.org#$#body{overflow:hidden !important}");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "example.org", "example.org#$?#body:{overflow:hidden !important}");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "example.org", "example.org#%#//scriptlet('abort-on-property-read', 'test')");
+        XCTAssertTrue(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "test.com", "example.org#%#//scriptlet('abort-on-property-read', 'test.com')");
+        XCTAssertFalse(result);
+
+        result = QuickAllowlistClipper().userRuleIsAssociated(with: "example.org", "example.org#%#window.__gaq = undefined;");
+        XCTAssertTrue(result);
     }
     
     static var allTests = [
@@ -381,5 +431,6 @@ final class QuickAllowlistClipperTests: XCTestCase {
         ("testReplaceRuleMethod", testReplaceRuleMethod),
         ("testAllowlistContains", testAllowlistContains),
         ("testInvertedAllowlistContains", testInvertedAllowlistContains),
+        ("testUserRuleIsAssociated", testUserRuleIsAssociated),
     ]
 }
