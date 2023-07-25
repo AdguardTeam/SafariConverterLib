6fd4440cb9fe313865cd00297e6f30d40c6249eb
Slava Leleka <v.leleka@adguard.com>
2023-07-25 19:41:59 +0300

    add cosmetic rule test

--- Tests/ContentBlockerConverterTests/Rules/CosmeticRuleTests.swift
+++ Tests/ContentBlockerConverterTests/Rules/CosmeticRuleTests.swift
@@ -97,6 +97,13 @@ final class CosmeticRuleTests: XCTestCase {
         XCTAssertEqual(result.permittedDomains, []);
         XCTAssertEqual(result.restrictedDomains, []);
         XCTAssertEqual(result.content, ".textad { visibility: hidden; }");
+        
+        result = try! CosmeticRule(ruleText: "med1.de#$#div[id^=\"imAd_\"] { visibility: hidden!important; }");
+
+        XCTAssertEqual(result.isInjectCss, true);
+        XCTAssertEqual(result.permittedDomains, ["med1.de"]);
+        XCTAssertEqual(result.restrictedDomains, []);
+        XCTAssertEqual(result.content, "div[id^=\"imAd_\"] { visibility: hidden!important; }");
     }
 
     func testCssRulesWhitelist() {
